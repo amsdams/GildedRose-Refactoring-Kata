53111b3883a436624eb893a9da1364944981c8be
Julien Langeveld <julien@kramphub.com>
2022-03-19 10:43:38 +0100

    refactor, added increase/decrease quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,66 +1,66 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+	private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	private static final String AGED_BRIE = "Aged Brie";
 	private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        //for (int i = 0; i < items.length; i++) {
-    	for(Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+	public void updateQuality() {
+		for (Item item : items) {
+			if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
 
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+				if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+					decreaseQuality(item);
+				}
+			} else {
+				increaseQuality(item);
+				if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+					
+					if (item.sellIn < 11) {
+						increaseQuality(item);
+					}
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+					if (item.sellIn < 6) {
+						increaseQuality(item);
+					}
+				}
+			}
 
-            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                item.sellIn = item.sellIn - 1;
-            }
+			if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+				item.sellIn = item.sellIn - 1;
+			}
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
-        }
-    }
+			if (item.sellIn < 0) {
+				if (!item.name.equals(AGED_BRIE)) {
+					if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+
+						if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+							decreaseQuality(item);
+						}
+					} else {
+						item.quality = item.quality - item.quality;
+					}
+				} else {
+					increaseQuality(item);
+				}
+			}
+		}
+	}
+
+	public void decreaseQuality(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+	}
+
+	public void increaseQuality(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+		}
+	}
 }
